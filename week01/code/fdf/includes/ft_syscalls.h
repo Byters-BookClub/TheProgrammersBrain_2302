e578426139848eba8437c897c9967cec07d87381
geneeol <trumpjqk@daum.net>
2023-02-01 20:57:23 +0900

    Feat: dahkang 코드 추가

---
+++ week01/code/fdf/includes/ft_syscalls.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_syscalls.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahkang <dahkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 00:25:04 by dahkang           #+#    #+#             */
+/*   Updated: 2022/12/23 01:09:44 by dahkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_SYSCALLS_H
+# define FT_SYSCALLS_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+void	ft_perror_exit(char *err_msg);
+void	*ft_malloc(size_t size);
+int		ft_open(const char *path, int oflag);
+void	ft_close(int fd);
+
+#endif
